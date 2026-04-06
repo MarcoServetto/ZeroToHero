0a324efec24d7463b5389d2bbd95ec8fb9dcc7d6
RolandTheHero <roland.sze@outlook.com>
2026-04-06 18:40:42 +1200

    Fix day 3 outro final image

--- src/levels/day03/Day3Outro.java
+++ src/levels/day03/Day3Outro.java
@@ -7,5 +7,5 @@ public class Day3Outro implements Function<Days.LevelName,String>{
   @Override public String apply(Days.LevelName name){
     return new htmlMangle.DirectInstructions(name,this.getClass().getSimpleName())
     // topStart/end   leftStart/end
-    .image(11)
+    .image(13)
     .build(); } }
